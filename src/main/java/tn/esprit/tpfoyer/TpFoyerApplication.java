3c70b7181f38cc02011c6e425fb17a117d12c2ee
medDhiaAlaya <dhiaalaya3@gmail.com>
2025-01-28 19:15:41 +0100

    first commit

---
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -0,0 +1,14 @@
+package tn.esprit.tpfoyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFoyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFoyerApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
